52ccdc6501387b9ee521d8abb4ad579e0fadd500
Paul Mitchum <paul@mile23.com>
2015-05-06 12:57:56 -0700

    NOW we can update fields

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -80,4 +80,73 @@ $edit = array($fieldname
     ));
   }
 
+  public function putUser(DruplexApplication $app, $uid) {
+    // Figure out if the user exists.
+    $user = user_load($uid);
+    if ($user) {
+//      error_log(print_r($user, TRUE));
+      $changed = FALSE;
+      // Work on schema fields.
+      $schema = \drupal_get_schema('users');
+      $schema = array_keys($schema['fields']);
+
+      $never_update_these = array('uid', 'init', 'data', 'pass', 'created');
+
+      $request = $app['request'];
+      // We rely on the before middleware to put JSON in the request.
+      foreach ($schema as $field) {
+        if (!in_array($field, $never_update_these)) {
+          if ($new_value = $request->get($field, FALSE)) {
+            $user->$field = $new_value;
+            $changed = TRUE;
+          }
+        }
+      }
+      unset($schema);
+      unset($never_update_these);
+
+      // Work on attached fields.
+      // ?fieldname=field_foo&fieldcolumn=value&fieldvalue=text
+      if (
+        !$request->get('fieldname', FALSE) ||
+        !$request->get('fieldcolumn', FALSE) ||
+        !$request->get('fieldvalue', FALSE)
+      ) {
+        $app->abort(400);
+        return;
+      }
+      // Gather the fields and schemata.
+      // FYI: Field API doesn't have CRUD.
+      $schemata = \drupal_get_schema();
+      $instances = \field_info_instances('user', 'user');
+      $fieldname = $request->get('fieldname');
+      foreach (array_keys($instances) as $field) {
+        if ($field == $fieldname) {
+          // Which column?
+          $fieldcolumn = $request->get('fieldcolumn');
+          $column = $fieldname . '_' . $fieldcolumn;
+          // Does the column exist in the schemata?
+          if (isset($schemata['field_data_' . $fieldname]['fields'][$column])) {
+            // Set the proper language code.
+            $language = \field_language('user', $user, $fieldname);
+            // Note that we completely ignore the concept of deltas.
+            $user->{$fieldname}[$language][0][$fieldcolumn] = $request->get('fieldvalue');
+            $changed = TRUE;
+          }
+        }
+      }
+      if ($changed) {
+        \user_save($user);
+        $user = \user_load($user->uid);
+        return $app->json(UserController::sanitize($user));
+      }
+
+      return $app->json(UserController::sanitize($user));
+    }
+    else {
+      $app->abort(404, 'No user for that id.');
+    }
+    $app->abort(400, "Wait, what?");
+  }
+
 }
